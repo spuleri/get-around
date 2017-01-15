c2b7570765a88f1016539dc79e9c47bc9b4d94fc
Sergio Puleri <spuleri@ufl.edu>
2017-01-15 13:40:43 -0500

    Add webpack and react

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # GetAround
-
+---
 A web app to aid in finding your way around a college campus as an international student.
+
+Setup
+---
+
+```
+npm install
+```
+
+
+
+Compile
+---
+
+```
+npm run compile
+```
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create component to produce some HTML
+
+const App = () => {
+  return <div>Hi!!</div>;
+}
+
+// Take this component's HTML and put it on the DOM
+ReactDOM.render(App);
